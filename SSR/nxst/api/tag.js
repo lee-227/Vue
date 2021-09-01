75d67964c27fb34d0b8fa471c19c5336a4ae169b
Lee <lee_227@163.com>
2021-09-01 09:48:05 +0800

    i

--- SSR/nxst/api/tag.js
+++
@@ -1,9 +0,0 @@
-import { request } from '@/plugins/axios'
-
-// 获取文章标签列表
-export const getTags = () => {
-  return request({
-    method: 'GET',
-    url: '/api/tags'
-  })
-}
--- SSR/nxst/middleware/notAuthenticated.js
+++
@@ -1,8 +0,0 @@
-/**
- * 已登录禁止访问登录页中间件
- */
-export default ({ store, redirect }) => {
-  if (store.state.user) {
-    return redirect('/')
-  }
-}
